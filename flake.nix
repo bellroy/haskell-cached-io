84d3c3eb03cdec7aaaace5d8ab2a876ca8f4a185
JackKelly-Bellroy <64521034+JackKelly-Bellroy@users.noreply.github.com>
2024-06-26 18:35:45 +1000

    Package updates 2024 06 (#20)
    * Use bellroy-nix-foss
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Added input 'bellroy-nix-foss':
        'github:bellroy/bellroy-nix-foss/b6c032e0c9aeb1886538d9db10809bc441dd2bac?narHash=sha256-7pEJ5a%2BhbpeCmKqRt%2BN5ddhsBD8IwDRenMN8WPnB8MQ%3D' (2024-06-25)
    • Added input 'bellroy-nix-foss/flake-compat':
        'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33?narHash=sha256-kvjfFW7WAETZlt09AgDn1MrtKzP7t90Vf7vypd3OL1U%3D' (2023-10-04)
    • Added input 'bellroy-nix-foss/flake-utils':
        'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a?narHash=sha256-SZ5L6eA7HJ/nmkzGG7/ISclqe6oZdOZTNoesiInkXPQ%3D' (2024-03-11)
    • Added input 'bellroy-nix-foss/flake-utils/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e?narHash=sha256-Vy1rq5AaRuLzOxct8nz4T6wlgyUR7zLU309k9mBC768%3D' (2023-04-09)
    • Added input 'bellroy-nix-foss/nixpkgs':
        'github:NixOS/nixpkgs/9693852a2070b398ee123a329e68f0dab5526681?narHash=sha256-jHJSUH619zBQ6WdC21fFAlDxHErKVDJ5fpN0Hgx4sjs%3D' (2024-06-22)
    • Removed input 'flake-utils'
    • Removed input 'flake-utils/systems'
    • Removed input 'nixpkgs'
    
    * Update cabal bounds
    
    * Update tested-with and regenerate CI

--- flake.nix
+++ flake.nix
@@ -2,53 +2,26 @@
   description = "A simple library to cache a single IO action with timeout";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    bellroy-nix-foss.url = "github:bellroy/bellroy-nix-foss";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      rec
-      {
-        # This becomes the list of package and devShell names.
-        supportedCompilers = [
-          "ghc88"
-          "ghc810"
-          "ghc90"
-          "ghc92"
-          "ghc94"
-          "ghc96"
-          "ghc98"
-        ];
-
-        packages = builtins.listToAttrs (
-          builtins.map
-            (compiler: {
-              name = compiler;
-              value = pkgs.haskell.packages.${compiler}.callPackage ./cached-io.nix { };
-            })
-            supportedCompilers
-        );
-
-        devShells' = (builtins.mapAttrs
-          (compiler: v: v.env.overrideAttrs (oldAttrs: {
-            buildInputs = oldAttrs.buildInputs
-              ++ [ pkgs.nixpkgs-fmt ]
-              ++ (with pkgs.haskell.packages.${compiler}; [
-              cabal-fmt
-              cabal-install
-              doctest
-              haskell-ci
-              haskell-language-server
-              hlint
-            ]);
-          }))
-          packages
-        );
-
-        devShells = devShells' // { default = devShells'.ghc92; };
-      });
+  outputs = inputs:
+    inputs.bellroy-nix-foss.lib.haskellProject {
+      cabalPackages = [
+        {
+          name = "cached-io";
+          path = ./cached-io.nix;
+        }
+      ];
+      supportedCompilers = [
+        "ghc810"
+        "ghc90"
+        "ghc92"
+        "ghc94"
+        "ghc96"
+        "ghc98"
+        "ghc910"
+      ];
+      defaultCompiler = "ghc96";
+    };
 }
